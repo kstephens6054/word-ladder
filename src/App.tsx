7f7ceb64ca42244bdd4a6a1709bd46bb41ee78c9
Kurt Stephens <kstephens6054@gmail.com>
2026-02-10 07:32:33 +0000

    Added dummy structure to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css'
-import Header from './components/Header/Header'
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header />
-    </>
-  )
+      <div className="content">
+        <p>The Playfield component goes here.</p>
+      </div>
+      <footer className="footer">
+        <p>The Footer component goes here.</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
