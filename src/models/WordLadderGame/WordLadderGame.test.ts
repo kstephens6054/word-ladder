1420354b87d26de996569b5b5004788ebd928f29
Kurt Stephens <kstephens6054@gmail.com>
2026-02-11 06:20:24 +0000

    Added WordLadderGame class

--- src/models/WordLadderGame/WordLadderGame.test.ts
+++ src/models/WordLadderGame/WordLadderGame.test.ts
@@ -1,9 +1,76 @@
+// @vitest-environment node
+
 import { describe, it, expect } from "vitest";
 import { WordLadderGame } from "./WordLadderGame";
 
 describe("WordLadderGame class", () => {
-  it("should exist", () => {
-    const game = new WordLadderGame();
-    expect(game).toBeDefined();
+  it("should initialize from a list of words", () => {
+    const wordList = ["cat", "cut", "cot", "got", "cog", "log", "dog"];
+    const game = new WordLadderGame(wordList);
+
+    expect([...game.words()]).toEqual(wordList);
+  });
+
+  it("should initialize with an empty path", () => {
+    const wordList = ["cat", "cut", "cot", "got", "cog", "log", "dog"];
+    const game = new WordLadderGame(wordList);
+
+    expect(game.fromWord).toBe("");
+    expect(game.toWord).toBe("");
+    expect(game.steps).toBe(0);
+  });
+
+  it("should initialize with a given path", () => {
+    const wordList = ["cat", "cut", "cot", "got", "cog", "log", "dog"];
+    const path = ["cat", "cot", "cog", "dog"];
+    const game = new WordLadderGame(wordList, path);
+
+    expect(game.fromWord).toBe("cat");
+    expect(game.toWord).toBe("dog");
+    expect(game.steps).toBe(4);
+  });
+
+  it("should return an iterator to its word list", () => {
+    const wordList = ["cat", "cut", "cot", "got", "cog", "log", "dog"];
+    const game = new WordLadderGame(wordList, [], 3);
+
+    expect([...game.words()]).toEqual(wordList);
+  });
+
+  it("should test if a word is in its word list", () => {
+    const wordList = ["cat", "cut", "cot", "got", "cog", "log", "dog"];
+    const game = new WordLadderGame(wordList, [], 3);
+
+    expect(game.hasWord("cat")).toBe(true);
+    expect(game.hasWord("foo")).toBe(false);
+  });
+
+  it("should start a new game with a random path", () => {
+    const wordList = [
+      "cat",
+      "hat",
+      "cut",
+      "hut",
+      "cot",
+      "hot",
+      "got",
+      "lot",
+      "cog",
+      "hog",
+      "log",
+      "fog",
+      "dog",
+    ];
+    const game = new WordLadderGame(wordList, [], 3);
+
+    expect(game.fromWord).toBe("");
+    expect(game.toWord).toBe("");
+    expect(game.steps).toBe(0);
+
+    game.startGame(4);
+
+    expect(game.fromWord).toBeOneOf(wordList);
+    expect(game.toWord).toBeOneOf(wordList);
+    expect(game.steps).toBe(4);
   });
 });
--- src/models/WordLadderGame/WordLadderGame.ts
+++ src/models/WordLadderGame/WordLadderGame.ts
@@ -1,3 +1,108 @@
+/**
+ * The WordLadderGame class implements the game logic given a list
+ * of words.
+ */
+import WordGraph from "../WordGraph";
+
 export class WordLadderGame {
-  constructor() {}
+  private _wordGraph: WordGraph;
+  private _path: string[];
+  private _minNeighbors: number;
+  private _startWords: string[];
+
+  /**
+   * Construct a new game instance.
+   *
+   * @param {sting[]} wordList The initial list of words
+   * @param {string[]} path Optional default path
+   * @param {number} minNeighbors Minimum neighbors for start words
+   */
+  constructor(
+    wordList: string[],
+    path: string[] = [],
+    minNeighbors: number = 1,
+  ) {
+    this._wordGraph = new WordGraph(wordList);
+    this._path = path;
+    this._minNeighbors = minNeighbors;
+    this._startWords = this._getStartWords();
+  }
+
+  /**
+   * Return an iterator to the list of words.
+   *
+   * @returns {MapIterator<string>}
+   */
+  words(): MapIterator<string> {
+    return this._wordGraph.words();
+  }
+
+  /**
+   * Test if a word is in the graph.
+   *
+   * @param {string} word
+   * @returns {boolean}
+   */
+  hasWord(word: string): boolean {
+    return this._wordGraph.hasWord(word);
+  }
+
+  /**
+   * Return the starting word for the current game. Returns an
+   * empty string if no path has been generated.
+   *
+   * @returns {string}
+   */
+  get fromWord(): string {
+    return this._path.length > 0 ? this._path[0]! : "";
+  }
+
+  /**
+   * Return the target word for the current game. Returns an
+   * empty string if no path has been generated.
+   *
+   * @returns {string}
+   */
+  get toWord(): string {
+    return this._path.length > 0 ? this._path.at(-1)! : "";
+  }
+
+  /**
+   * Returns the path length for the current game.
+   *
+   * @returns {number}
+   */
+  get steps(): number {
+    return this._path.length;
+  }
+
+  /**
+   * Generate a random path of given length from the word graph.
+   *
+   * @param {number} length
+   */
+  startGame(length: number): void {
+    let path: string[] = [];
+
+    while (path.length < length) {
+      let startWord = this._getRandomStartWord();
+      if (startWord === "") break;
+
+      path = this._wordGraph.findRandomPath(startWord, length);
+    }
+
+    this._path = path;
+  }
+
+  private _getRandomStartWord(): string {
+    const randomIndex = Math.floor(this._startWords.length * Math.random());
+    return this._startWords[randomIndex];
+  }
+
+  private _getStartWords(): string[] {
+    return Array.from(this.words()).filter((word) => {
+      const neighbors = this._wordGraph.getNeighbors(word)!;
+      return neighbors.length >= this._minNeighbors;
+    });
+  }
 }
