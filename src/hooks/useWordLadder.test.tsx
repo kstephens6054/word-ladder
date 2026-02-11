6a391d598ac3ddabda4be03132154481f703e3de
Kurt Stephens <kstephens6054@gmail.com>
2026-02-11 09:44:28 +0000

    Added useWordLladder hook

--- src/hooks/useWordLadder.test.tsx
+++ src/hooks/useWordLadder.test.tsx
@@ -1,24 +1,108 @@
 import { describe, it, expect } from "vitest";
-import useWordLadder from "./useWordLadder";
+import { screen, render } from "@testing-library/react";
+import { useWordLadder } from "./useWordLadder";
 
 describe("useWordLadder hook", () => {
-  it("should return the current game properties", () => {
-    const { game } = useWordLadder([]);
-    expect(game).toBeDefined();
-    expect(game).toHaveProperty("fromWord");
-    expect(game).toHaveProperty("toWord");
-    expect(game).toHaveProperty("steps");
+  it("should initialize with an empty path", () => {
+    const TestApp = () => {
+      const { fromWord, toWord, steps } = useWordLadder({ wordList: [] });
+      return (
+        <>
+          <div>{`fromWord = "${fromWord}"`}</div>
+          <div>{`toWord = "${toWord}"`}</div>
+          <div>{`steps = ${steps}`}</div>
+        </>
+      );
+    };
+
+    render(<TestApp />);
+
+    expect(screen.getByText('fromWord = ""')).toBeInTheDocument();
+    expect(screen.getByText('toWord = ""')).toBeInTheDocument();
+    expect(screen.getByText("steps = 0")).toBeInTheDocument();
+  });
+
+  it("should initialize from the given path", () => {
+    const TestApp = () => {
+      const { fromWord, toWord, steps } = useWordLadder({
+        wordList: ["cat", "cot", "cog", "dog"],
+        path: ["cat", "cot", "cog", "dog"],
+      });
+      console.log(fromWord, toWord, steps);
+      return (
+        <>
+          <div>{`fromWord = "${fromWord}"`}</div>
+          <div>{`toWord = "${toWord}"`}</div>
+          <div>{`steps = ${steps}`}</div>
+        </>
+      );
+    };
+
+    render(<TestApp />);
+
+    expect(screen.getByText('fromWord = "cat"')).toBeInTheDocument();
+    expect(screen.getByText('toWord = "dog"')).toBeInTheDocument();
+    expect(screen.getByText("steps = 4")).toBeInTheDocument();
   });
 
   it("should test if the word is in the list", () => {
-    const { hasWord } = useWordLadder(["cat", "cot"]);
-    expect(hasWord("cat")).toBe(true);
-    expect(hasWord("cot")).toBe(true);
-    expect(hasWord("foo")).toBe(false);
+    const word1 = "cat";
+    const word2 = "fat";
+
+    const TestApp = () => {
+      const { hasWord } = useWordLadder({
+        wordList: ["cat", "cot", "cog", "dog"],
+      });
+      return (
+        <>
+          <div>
+            {`${word1} ${hasWord(word1) ? "is" : "is not"} in the list`}
+          </div>
+          <div>
+            {`${word2} ${hasWord(word2) ? "is" : "is not"} in the list`}
+          </div>
+        </>
+      );
+    };
+
+    render(<TestApp />);
+
+    expect(screen.getByText(`${word1} is in the list`)).toBeInTheDocument();
+    expect(screen.getByText(`${word2} is not in the list`)).toBeInTheDocument();
   });
 
-  it("should return the areNeigbors, function", () => {
-    const { areNeighbors } = useWordLadder([]);
-    expect(areNeighbors("cat", "cot")).toBe(true);
+  it("should test if two words are neighbors", () => {
+    const word1 = "cat";
+    const word2 = "fat";
+    const word3 = "fan";
+
+    const TestApp = () => {
+      const { areNeighbors } = useWordLadder({
+        wordList: ["cat", "cot", "cog", "dog"],
+      });
+      return (
+        <>
+          <div>
+            {`${word1} and ${word2} ${
+              areNeighbors(word1, word2) ? "are" : "are not"
+            } neighbors`}
+          </div>
+          <div>
+            {`${word1} and ${word3} ${
+              areNeighbors(word1, word3) ? "are" : "are not"
+            } neighbors`}
+          </div>
+        </>
+      );
+    };
+
+    render(<TestApp />);
+
+    expect(
+      screen.getByText(`${word1} and ${word2} are neighbors`),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(`${word1} and ${word3} are not neighbors`),
+    ).toBeInTheDocument();
   });
 });
--- src/hooks/useWordLadder.tsx
+++ src/hooks/useWordLadder.tsx
@@ -1,36 +1,95 @@
-import WordGraph from "../models/WordGraph";
+import { useState } from "react";
+import { WordLadderGame } from "../models/WordLadderGame";
 
-export type WordLadderGame = {
-  game: {
-    fromWord: string;
-    toWord: string;
-    steps: number;
-  };
-  hasWord: (word: string) => boolean;
-  areNeighbors: (word1: string, word2: string) => boolean;
+export type UseWordLadderProperties = {
+  wordList: string[];
+  path?: string[];
+  minNeighbors?: number;
 };
 
 /**
+ * The useWordLadder hook provides the game logic for the Word Ladder
+ * game.
  *
- * @param wordList
- * @returns
+ * @param {object} props
+ * @param {string[]} props.wordList
+ * @param {string[]} props.path
+ * @param {number} props.minNeighbors
+ *
+ * @returns {object}
  */
+const useWordLadder = ({
+  wordList,
+  path,
+  minNeighbors,
+}: UseWordLadderProperties) => {
+  const [game, setGame] = useState<WordLadderGame>(
+    () => new WordLadderGame(wordList, path, minNeighbors),
+  );
 
-const useWordLadder = (wordList: string[]): WordLadderGame => {
-  const game = {
-    fromWord: "cat",
-    toWord: "dog",
-    steps: 4,
+  const [fromWord, setFromWord] = useState<string>(() => game.fromWord);
+  const [toWord, setToWord] = useState<string>(() => game.toWord);
+  const [steps, setsteps] = useState<number>(() => game.steps);
+
+  const _updateStatus = (): void => {
+    setFromWord(() => game.fromWord);
+    setToWord(() => game.toWord);
+    setsteps(() => game.steps);
   };
 
-  const wordGraph = new WordGraph(wordList);
+  /**
+   * Re-initialize the game with a new word list. Refer to the
+   * useWordLadder initializers for details.
+   *
+   * @param {UseWordLadderProperties}
+   */
+  const newGame = ({
+    wordList,
+    path,
+    minNeighbors,
+  }: UseWordLadderProperties): void => {
+    setGame(() => new WordLadderGame(wordList, path, minNeighbors));
+    _updateStatus();
+  };
 
+  /**
+   * Start a new game with a random path. This forces an update
+   * of the game status.
+   *
+   * @param {number} steps Number of steps for the random path
+   */
+  const startGame = (steps: number): void => {
+    game.startGame(steps);
+    _updateStatus();
+  };
+
+  /**
+   * Test if two words are neighbors.
+   *
+   * @param {string} word1
+   * @param {string} word2
+   * @returns {boolean}
+   */
   const areNeighbors = (word1: string, word2: string): boolean =>
-    WordGraph.areNeighbors(word1, word2);
+    game.areNeighbors(word1, word2);
 
-  const hasWord = (word: string): boolean => wordGraph.hasWord(word);
+  /**
+   * Is a word in theh word list?
+   *
+   * @param {string} word
+   * @returns {boolean}
+   */
+  const hasWord = (word: string): boolean => game.hasWord(word);
 
-  return { game, hasWord, areNeighbors };
+  return {
+    fromWord,
+    toWord,
+    steps,
+    newGame,
+    startGame,
+    hasWord,
+    areNeighbors,
+  } as const;
 };
 
-export default useWordLadder;
+export { useWordLadder };
